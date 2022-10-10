cdfb048b845e6b2b81fd0c781ce52f8f853be12d
Veper(Mac) <veper.chesley@protonmail.com>
2022-10-10 22:44:10 +0800

    docs(yarn-eject): git commit and push before yarn eject

--- App/src/carPark.tsx
+++ App/src/carPark.tsx
@@ -1,10 +1,5 @@
 import axios from "axios";
-import {
-    Box,
-    Button,
-    Icon,
-    Text,
-} from "native-base";
+import { Box, Button, Icon, Text } from "native-base";
 import React, { useEffect, useMemo, useState } from "react";
 import { TouchableOpacity } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
@@ -13,31 +8,33 @@ import CarParkInfoAPI from "./components/carParkInfoAPI";
 import CarParkVacancyAPI from "./components/carParkVacancyAPI";
 import Spacer from "./components/spacer";
 
-//console.log(`${process.env.REACT_NATIVE_APP_EXPRESS_API}/carParkInfo`)
+console.log(`${process.env.REACT_NATIVE_APP_EXPRESS_API}`);
 
 function apiRequest() {
-    axios.post(`${process.env.REACT_NATIVE_APP_EXPRESS_API}/carParkInfo`)
-        .then((response) => {
-            console.log(response.data)
-        })
-        .catch((error) => {
-            console.error({ error })
-        })
+  axios(`${process.env.REACT_NATIVE_APP_EXPRESS_API}/carParkInfo`, {
+    method: "POST",
+  })
+    .then((response) => {
+      console.log(response.data);
+    })
+    .catch((error) => {
+      console.error({ error });
+    });
 }
 
 function CarPark() {
-    return (
-        <SafeAreaView>
-            <Button bg="amber.400" onPress={apiRequest}>
-                <Text color="darkBlue.500">Button</Text>
-            </Button>
-            <Box bg="dark.50">
-                <CarParkDistrict />
-                <Spacer />
-                <CarParkInfoAPI />
-            </Box>
-        </SafeAreaView>
-    );
+  return (
+    <SafeAreaView>
+      <Button bg="amber.400" onPress={apiRequest}>
+        <Text color="darkBlue.500">Button</Text>
+      </Button>
+      <Box bg="dark.50">
+        <CarParkDistrict />
+        <Spacer />
+        <CarParkInfoAPI />
+      </Box>
+    </SafeAreaView>
+  );
 }
 
 export default CarPark;
--- express-server/main.ts
+++ express-server/main.ts
@@ -1,10 +1,8 @@
 import express from 'express';
+import runningAt from 'running-at';
 import cors from 'cors';
 import axios from 'axios';
-import { createClient } from 'redis';
 import { MongoClient } from 'mongodb';
-
-const redisClient = createClient();
 
 
 const app = express();
@@ -15,6 +13,10 @@ app.use(cors());
 // const corsOptions = {
 //    origin : 'localhost:19000'
 // }
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+})
 
 app.post('/carParkInfo', async (req, res, next) => {
     console.log({ 'POST': 'Frontend post request received!' })
@@ -24,110 +26,77 @@ app.post('/carParkInfo', async (req, res, next) => {
     const data = response.data.results;
     //console.log(data)
 
-    async function runMongo() {
+    // async function runMongo() {
 
-        const dbName = "carPark";
-        const collectionName = "carParkInfo";
+    //     const dbName = "carPark";
+    //     const collectionName = "carParkInfo";
 
-        const uri = "mongodb+srv://m001-student:m001-student@sandbox.cgiqc.mongodb.net/?retryWrites=true&w=majority";
-        const mongoClient = new MongoClient(uri, { monitorCommands: true });
+    //     const uri = "mongodb+srv://m001-student:m001-student@sandbox.cgiqc.mongodb.net/?retryWrites=true&w=majority";
+    //     const mongoClient = new MongoClient(uri, { monitorCommands: true });
 
-        const db = mongoClient.db(dbName);
-        const collectionData = await db.listCollections().toArray();
-        console.log({ collectionData })
+    //     const db = mongoClient.db(dbName);
+    //     const collectionData = await db.listCollections().toArray();
+    //     console.log({ collectionData })
 
-        if (collectionData.length > 0) {
-            const existCollectionName = collectionData[0].name
-            console.log({ existCollectionName });
-            const checkCollectionExist = (existCollectionName == collectionName);
-            console.log({ checkCollectionExist })
+    //     if (collectionData.length > 0) {
+    //         const existCollectionName = collectionData[0].name
+    //         console.log({ existCollectionName });
+    //         const checkCollectionExist = (existCollectionName == collectionName);
+    //         console.log({ checkCollectionExist })
 
+    //         try {
+    //             console.log("connect mongoDB successfully!");
 
-            try {
-                console.log("connect mongoDB successfully!");
+    //             if (checkCollectionExist) {
+    //                 const db = mongoClient.db("carPark");
+    //                 const collection = db.collection('carParkInfo');
 
-                if (checkCollectionExist) {
-                    const db = mongoClient.db("carPark");
-                    const collection = db.collection('carParkInfo');
+    //                 const result = await collection.drop();
+    //                 console.log("collection drop successfully!");
+    //             }
 
-                    const result = await collection.drop();
-                    console.log("collection drop successfully!");
-                }
+    //             const db = mongoClient.db("carPark");
+    //             const collection = db.collection('carParkInfo');
 
+    //             const result = await collection.insertMany(data)
+    //             console.log(
+    //                 `inserted data : ${result.insertedCount}`
+    //             );
 
-                const db = mongoClient.db("carPark");
-                const collection = db.collection('carParkInfo');
+    //         } finally {
+    //             console.log("disconnecting mongoDB...")
+    //             await mongoClient.close();
+    //             console.log("mongodb disconnected!")
+    //         }
+    //     }
+    //     else {
+    //         try {
+    //             console.log("connect mongoDB successfully!");
+    //             const db = mongoClient.db("carPark");
+    //             const collection = db.collection('carParkInfo');
 
-                const result = await collection.insertMany(data)
-                console.log(
-                    `inserted data : ${result.insertedCount}`
-                );
+    //             const result = await collection.insertMany(data)
+    //             console.log(
+    //                 `inserted data : ${result.insertedCount}`
+    //             );
 
-            } finally {
-                console.log("disconnecting mongoDB...")
-                await mongoClient.close();
-                console.log("mongodb disconnected!")
-            }
-        }
-        else {
-            try {
-                console.log("connect mongoDB successfully!");
-                const db = mongoClient.db("carPark");
-                const collection = db.collection('carParkInfo');
+    //         }
+    //         finally {
+    //             console.log("disconnecting mongoDB...")
+    //             await mongoClient.close();
+    //             console.log("mongodb disconnected!")
+    //         }
+    //     }
+    // }
+    // runMongo().catch((err) => {
+    //     console.error({ err })
+    // })
 
-                const result = await collection.insertMany(data)
-                console.log(
-                    `inserted data : ${result.insertedCount}`
-                );
-
-            }
-            finally {
-                console.log("disconnecting mongoDB...")
-                await mongoClient.close();
-                console.log("mongodb disconnected!")
-            }
-        }
-
-
-        // try {
-        //         console.log("connect mongoDB successfully!");
-
-        //         const db = mongoClient.db("carPark");
-        //         const collection = db.collection('carParkInfo');
-
-        //         const result = await collection.insertMany(data)
-        //         console.log(
-        //             `inserted data : ${result.insertedCount}`
-        //         );
-
-        //     }
-
-    }
-    runMongo().catch((err) => {
-        console.error({ err })
-    })
-
-    //redisClient.on('error', (err) => console.error("Redis Error : ", err))
-    //await client.connect();
-
-    /*
-    try {
-        await data.forEach((item: any) => {
-            redisClient.set(`${item["district"]}`, `${item}`)
-        })
-    }
-    catch (err) {
-        console.error("Redis set Error : ", err);
-    }
-    */
-
-    //const dataInfo = await redisClient.get('元朗區');
-
-    //await redisClient.disconnect();
     res.json({ 'POST': 'response' })
 })
 
 app.listen(PORT, () => {
+    console.log(runningAt(PORT))
     try {
         console.log(`server is started , PORT listening to ${PORT}`);
     }
