19e3dd8fd03e7a568e2658a19e9335cbbc5791c5
Veper(Mac) <veper.chesley@protonmail.com>
2022-12-11 18:01:32 +0800

    style(change-bottom-detail-box-style): *

--- App/src/components/carParkInfoAPI.tsx
+++ App/src/components/carParkInfoAPI.tsx
@@ -4,12 +4,12 @@ import { Box, HStack, Image, ScrollView, Spinner, Text, View, VStack } from 'nat
 import { useAppSelector } from './redux/hooks';
 import { TouchableOpacity } from 'react-native';
 
-function CarParkInfoAPI({ selRef }: any) {
-	const selector = useAppSelector((state) => state.district);
+function CarParkInfoAPI() {
+  const selector = useAppSelector((state) => state.district);
 
-	return (
-		<>
-			{/*}			<Text bg="cyan.900">
+  return (
+    <>
+      {/*}			<Text bg="cyan.900">
 				{selector.loading == 'is rejected' ? (
 					<Text color="red.500">Status : {selector.loading}</Text>
 				) : selector.loading == 'is pending' ? (
@@ -19,116 +19,105 @@ function CarParkInfoAPI({ selRef }: any) {
 				)}
 			</Text> */}
 
-			{selector.loading == 'is fulfilled' ? (
-				<ScrollView
-					horizontal
-					style={{
-						position: 'absolute',
-						bottom: 50,
-						width: '100%',
-					}}
-				>
-					{selector.districtData.map((item: any) => (
-						<>
-							<TouchableOpacity
-								key={item.park_Id}
-								onPress={() => selRef(item.latitude, item.longitude)}
-								style={{
-									zIndex: 1,
-									height: 250,
-									opacity: 0.8,
-									backgroundColor: 'silver',
-									borderWidth: 2,
-									borderColor: 'white',
-									borderRadius: 10,
-									marginHorizontal: 5,
-									paddingHorizontal: 15,
-									paddingVertical: 15,
-								}}
-							>
-								<VStack>
-									<Text color="dark.900" fontSize="md" fontWeight="bold">
-										{' '}
-										{item.name}{' '}
-									</Text>
-									<HStack>
-										<Text color="dark.900" fontWeight="bold" fontSize="sm">
-											地址：
-										</Text>
-										<Text color="dark.900" fontWeight="bold" fontSize="sm">
-											{item.displayAddress}
-										</Text>
-									</HStack>
-									{item?.renditionUrls?.carpark_photo ? (
-										<>
-											<Text color="dark.900" fontWeight="bold" fontSize="sm">
-												位置圖片：
-											</Text>
-											<Image
-												source={{ uri: `${item?.renditionUrls?.carpark_photo}` }}
-												size="lg"
-												alt="Text"
-											/>
-										</>
-									) : (
-										<>
-											<Text color="dark.900" fontWeight="bold" fontSize="sm">
-												位置圖片：
-											</Text>
-											<Text color="red.400" fontWeight="bold" fontSize="sm">
-												資料沒有提供
-											</Text>
-										</>
-									)}
+      {selector.loading == 'is fulfilled' ? (
+        <ScrollView
+          horizontal
+          style={{
+            position: 'absolute',
+            bottom: 50,
+            width: '100%',
+          }}
+        >
+          {selector.districtData.map((item: any) => (
+            <>
+              <TouchableOpacity
+                key={item.park_Id}
+                style={{
+                  zIndex: 1,
+                  height: 250,
+                  opacity: 0.8,
+                  backgroundColor: 'white',
+                  borderRadius: 10,
+                  marginHorizontal: 5,
+                  paddingHorizontal: 15,
+                  paddingVertical: 15,
+                }}
+              >
+                <VStack space={5}>
+                <HStack space={5}>
+                  {item?.renditionUrls?.carpark_photo ? (
+                    <Image
+                      source={{ uri: `${item?.renditionUrls?.carpark_photo}` }}
+                      borderRadius={10}
+                      size="xl"
+                      alt="image"
+                    />
+                  ) : null}
 
-									{item?.paymentMethods ? (
-										<HStack>
-											<Text color="dark.900" fontSize="lg" fontWeight="bold">
-												付款方式：
-											</Text>
-											<Text color="amber.400" fontSize="lg" fontWeight="bold">
-												{[item?.paymentMethods].join()}
-											</Text>
-										</HStack>
-									) : (
-										<HStack>
-											<Text color="dark.900" fontSize="sm" fontWeight="bold">
-												付款方式：
-											</Text>
-											<Text color="red.400" fontSize="sm" fontWeight="bold">
-												資料沒有提供
-											</Text>
-										</HStack>
-									)}
+                  <VStack space={5}>
+                    <Text color="dark.50" fontSize="md" fontWeight="bold">
+                      {' '}
+                      {item.name}{' '}
+                    </Text>
+                    <HStack>
+                      <Text color="dark.50" fontWeight="bold" fontSize="sm">
+                        地址：
+                      </Text>
+                      <Text color="dark.50" fontWeight="bold" fontSize="sm">
+                        {item.displayAddress}
+                      </Text>
+                    </HStack>
+                    {item?.paymentMethods ? (
+                      <HStack>
+                        <Text color="dark.50" fontSize="lg" fontWeight="bold">
+                          付款方式：
+                        </Text>
+                        <Text color="amber.400" fontSize="lg" fontWeight="bold">
+                          {[item?.paymentMethods].join()}
+                        </Text>
+                      </HStack>
+                    ) : (
+                      <HStack>
+                        <Text color="dark.50" fontSize="sm" fontWeight="bold">
+                          付款方式：
+                        </Text>
+                        <Text color="red.400" fontSize="sm" fontWeight="bold">
+                          資料沒有提供
+                        </Text>
+                      </HStack>
+                    )}
 
-									<HStack>
-										<CarParkVacancyAPI Id={item.park_Id} />
-									</HStack>
-								</VStack>
-							</TouchableOpacity>
-						</>
-					))}
-				</ScrollView>
-			) : (
-				<View
-					style={{
-						height: '100%',
-						display: 'flex',
-						justifyContent: 'center',
-						alignItems: 'center',
-					}}
-				>
-					<View style={{ marginBottom: '50%' }}>
-						<Spinner size="lg" />
-						<Text color="primary.500" fontSize="lg" fontWeight="bold">
-							{' '}
-							Loading{' '}
-						</Text>
-					</View>
-				</View>
-			)}
-		</>
-	);
+                  </VStack>
+                </HStack>
+
+                    <HStack>
+                      <CarParkVacancyAPI Id={item.park_Id} />
+                    </HStack>
+                </VStack>
+              </TouchableOpacity>
+            </>
+          ))}
+        </ScrollView>
+      ) : (
+        <View
+          style={{
+            height: '100%',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <View style={{ marginBottom: '50%' }}>
+            <Spinner size="lg" />
+            <Text color="primary.500" fontSize="lg" fontWeight="bold">
+              {' '}
+              Loading{' '}
+            </Text>
+          </View>
+        </View>
+      )}
+    </>
+  );
 }
 
 export default CarParkInfoAPI;
--- App/src/components/carParkMap.tsx
+++ App/src/components/carParkMap.tsx
@@ -9,58 +9,76 @@ import { useAppSelector } from './redux/hooks';
 import { fetchMap } from './redux/mapReducer';
 
 function CarParkMap() {
-	const [opened, toggle]: boolean = useReducer((opened: boolean) => !opened, false);
+  const [opened, toggle]: boolean = useReducer((opened: boolean) => !opened, false);
 
-	const selector = useAppSelector((state) => state.district);
+  const selector = useAppSelector((state) => state.district);
 
-	//console.log('data : ', selector.mapData);
-	//console.log('loading : ', selector.loading);
+  //console.log('data : ', selector.mapData);
+  //console.log('loading : ', selector.loading);
+  //
 
-	const styles = StyleSheet.create({
-		container: {
-			backgroundColor: '#fff',
-			alignItems: 'center',
-			justifyContent: 'center',
-		},
-		map: {
-			width: Dimensions.get('window').width,
-			height: Dimensions.get('window').height,
-		},
-	});
+  const fetchData = useCallback(
+    (id: string) => {
+      console.log('ID received : ', id);
+      axios(
+        `https://api.data.gov.hk/v1/carpark-info-vacancy?data=vacancy&carparkIds=${id}&lang=zh_TW`
+      ).then((result) => {
+        const info = result.data.results[0];
+        const privateCarInfo = info.privateCar;
+        const privateCarVacancy = privateCarInfo[0];
 
-	return (
-		<>
-			<View style={styles.container}>
-				<MapView
-					style={styles.map}
-					showsScale={true}
-					showsCompass={true}
-					//showsUserLocation={true}
-					//followsUserLocation={true}
-					//userLocationCalloutEnabled={true}
-					showsMyLocationButton={true}
-				>
-					{selector.loading == 'is fulfilled'
-						? selector.districtData.map((item: any) => (
-								<Marker
-									key={item.park_Id}
-									title={item.name}
-									description={item?.displayAddress}
-									onPress={toggle}
-									coordinate={{ latitude: item.latitude, longitude: item.longitude }}
-								>
-									{/*}<Image
+        //console.log(privateCarVacancy.vacancy);
+        return privateCarVacancy.vacancy.toString();
+      });
+    },
+    [selector.districtData]
+  );
+
+  const styles = StyleSheet.create({
+    container: {
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    map: {
+      width: Dimensions.get('window').width,
+      height: Dimensions.get('window').height,
+    },
+  });
+
+  return (
+    <>
+      <View style={styles.container}>
+        <MapView
+          style={styles.map}
+          showsScale={true}
+          showsCompass={true}
+          //showsUserLocation={true}
+          //followsUserLocation={true}
+          //userLocationCalloutEnabled={true}
+          showsMyLocationButton={true}
+        >
+          {selector.loading == 'is fulfilled'
+            ? selector.districtData.map((item: any) => (
+              <Marker
+                key={item.park_Id}
+                title={`${item.name} 車位：${fetchData(item.park_Id)}`}
+                description={`${item?.displayAddress}`}
+                onPress={toggle}
+                coordinate={{ latitude: item.latitude, longitude: item.longitude }}
+              >
+                {/*}<Image
 										source={item?.renditionUrls?.carpark_photo}
 										style={{ height: 6, width: 6 }}
 									/>*/}
-								</Marker>
-						  ))
-						: null}
-				</MapView>
-				{/* <CarParkMapDetailDisplay popout={opened} /> */}
-			</View>
-		</>
-	);
+              </Marker>
+            ))
+            : null}
+        </MapView>
+        {/* <CarParkMapDetailDisplay popout={opened} /> */}
+      </View>
+    </>
+  );
 }
 
 export default CarParkMap;
