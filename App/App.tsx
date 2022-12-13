7582fd66c8498ccbfaf84a3ecf202e7e9283218b
Veper(Mac) <veper.chesley@protonmail.com>
2022-12-13 19:54:16 +0800

    fix(update-expo-SDK-version-44-->-45): reinstall dependencies from updated yarn.lock file
    disable yellow box warning log from bottom on simulator

--- App/App.tsx
+++ App/App.tsx
@@ -1,38 +1,35 @@
-import React from "react";
-import { SafeAreaView } from "react-native";
-import { NativeBaseProvider, extendTheme } from "native-base";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import Home from "./src/home";
+import React from 'react';
+import { SafeAreaView } from 'react-native';
+import { NativeBaseProvider, extendTheme } from 'native-base';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Home from './src/home';
 
 const RootStack = createNativeStackNavigator();
 const Tabs = createBottomTabNavigator();
 
 // Define the config
 const config = {
-  useSystemColorMode: true,
-  initialColorMode: "light",
+	useSystemColorMode: true,
+	initialColorMode: 'light',
 };
 
 // extend the theme
 export const theme = extendTheme({ config });
 type MyThemeType = typeof theme;
-declare module "native-base" {
-  interface ICustomTheme extends MyThemeType {}
+declare module 'native-base' {
+	interface ICustomTheme extends MyThemeType {}
 }
 export default function App() {
-  return (
-    <NativeBaseProvider>
-      <NavigationContainer>
-        <RootStack.Navigator>
-          <RootStack.Screen
-            name="Home"
-            component={Home}
-            options={{ headerShown: false }}
-          />
-        </RootStack.Navigator>
-      </NavigationContainer>
-    </NativeBaseProvider>
-  );
+	console.disableYellowBox = true;
+	return (
+		<NativeBaseProvider>
+			<NavigationContainer>
+				<RootStack.Navigator>
+					<RootStack.Screen name="Home" component={Home} options={{ headerShown: false }} />
+				</RootStack.Navigator>
+			</NavigationContainer>
+		</NativeBaseProvider>
+	);
 }
